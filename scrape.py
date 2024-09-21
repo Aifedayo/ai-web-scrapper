a0f1fd083b948ad0a8ef8a78e5ce482f836319df
Aifedayo <akeemifedayolag@gmail.com>
2024-09-21 21:47:03 +0100

    Splitting the dom_content into chunks for the LLM

--- scrape.py
+++ scrape.py
@@ -1,12 +1,15 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+import os
+from bs4 import BeautifulSoup
 
 def scrape_website(website):
-    print("Launching chrme browser...")
+    print("Launching chrome browser...")
 
-    chrome_driver_path = ""
+    chrome_driver_path = os.path.abspath("./chromedriver")
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     try:
@@ -16,6 +19,38 @@ def scrape_website(website):
         time.sleep(10)
 
         return html
+    except Exception as e:
+        print(f"Failed to load the page: {e}")
     finally:
         driver.quit()
+
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip())
     
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    # Split the DOM content into chunks of size max_length
+    chunks = []
+    for i in range(0, len(dom_content), max_length):
+        chunk = dom_content[i: i+max_length]
+        chunks.append(chunk)
+    return chunks
+    # return [
+    #     dom_content[i: i+max_length] for i in range(0, len(dom_content), max_length)
+    # ]
